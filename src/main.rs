6bf91d194bf04a7aff03ffc49b1cd97c98d1f456
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-02 01:22:01 +0100

    basic looping and arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn add(n1: i32, n2: i32) -> i32 {
+    n1 + n2
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    let values = [8, 7, 5, 3, 0];
+    let mut sum = 0;
+
+    for n in values {
+        sum = add(sum,  n);
+    }
+
+    print!("Sum: {}", sum);
 }
