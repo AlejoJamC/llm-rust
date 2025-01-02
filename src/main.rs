ac8ca87f6c154795310456bf98138a22ea8e086b
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-02 01:46:06 +0100

    array slices

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ fn add(n1: i32, n2: i32) -> i32 {
     n1 + n2
 }
 
-
 fn main() {
     println!("Hello, world!");
 
     let values = [8, 7, 5, 3, 0];
     let mut sum = 0;
 
-    for n in values {
-        sum = add(sum,  n);
+    // Slices in rust
+    for n in &values[0..2]{
+        sum = add(sum, *n);
+    }
+
+    for n in &values[2..]{  // Slice from 2 to end  
+        sum = add(sum,  *n);
     }
 
     print!("Sum: {}", sum);
