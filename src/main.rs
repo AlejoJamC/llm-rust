d338fec35218e91e69316ccaea53cf262a829b41
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-08 00:34:07 +0100

    std input lib basic message printer

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,10 @@
-#[derive(Debug)]
-struct Accumulator{
-    sum: i32,
-}
+use std::io::stdin;
 
-impl Accumulator{
-    fn new(sum:i32 ) -> Self{
-        Self{sum}
-    }
+fn main() {
+    let mut message = String::new();
+    println!("Enter your message: ");
 
-    fn get(self) -> i32{
-        // access properties and methods of the struct
-        self.sum
-    }
 
-    fn add(n1: i32, n2: i32) -> i32{
-        let mut sum = n1;
-        let (count, increment) = if n2 > 0 {(n2, 1)} else {(-n2, -1)};
-        let mut handles = vec![];
-
-        for _ in 0..count {
-            handles.push(
-                thread::spawn( || {
-                    sum += increment;
-                })
-            );
-            sum += increment;
-        }
-
-        for handle in handles{
-            handle.join().unwrap();
-        }
-
-        sum
-    }
-}
-
-fn main(){
-    let acc = Accumulator::new(0);
-    let sum = Accumulator::add(0, 10);
-    println!("Sum: {}", sum);
+    stdin().read_line(&mut message).unwrap();
+    println!("Message received: {}", message);
 }
