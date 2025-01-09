e56667815c2a54335b7f2f9de11529d6b572bddd
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-09 23:59:09 +0100

    basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-use std::io::stdin;
+use std::net::TcpListener;
+use std::io::{Read, Write};
 
-fn main() {
-    let mut message = String::new();
-    println!("Enter your message: ");
+fn main() -> std::io::Result<()> {
+    // Escuchar en el puerto 7878
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Servidor HTTP escuchando en http://127.0.0.1:7878");
 
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+        println!("Nueva conexión establecida!");
 
-    stdin().read_line(&mut message).unwrap();
-    println!("Message received: {}", message);
+        // Leer la solicitud HTTP
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer)?;
+
+        // Imprimir la solicitud HTTP en la consola
+        println!("Solicitud:\n{}", String::from_utf8_lossy(&buffer[..]));
+
+        // Crear la respuesta HTTP
+        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
+        stream.write(response.as_bytes())?;
+        stream.flush()?;
+    }
+
+    Ok(())
 }
