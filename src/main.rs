572cd5bc262a87385fa1bbdb7f26c4f058a7fc30
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-01-15 20:05:27 +0100

    Adding tokio to the project version 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
-use std::net::TcpListener;
-use std::io::{Read, Write};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() -> std::io::Result<()> {
-    // Escuchar en el puerto 7878
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
-    println!("Servidor HTTP escuchando en http://127.0.0.1:7878");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    for stream in listener.incoming() {
-        let mut stream = stream?;
-        println!("Nueva conexión establecida!");
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
-        // Leer la solicitud HTTP
-        let mut buffer = [0; 1024];
-        stream.read(&mut buffer)?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
 
-        // Imprimir la solicitud HTTP en la consola
-        println!("Solicitud:\n{}", String::from_utf8_lossy(&buffer[..]));
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
 
-        // Crear la respuesta HTTP
-        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
-        stream.write(response.as_bytes())?;
-        stream.flush()?;
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
     }
-
-    Ok(())
 }
